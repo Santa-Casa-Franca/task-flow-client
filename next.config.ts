1aa8c4892c7a37e4470bed60bf219b337cf567f7
Antonio Alves <antonioalves.dev@outlook.com>
2025-03-18 09:37:21 -0300

    add alter variable

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+      NEXT_PUBLIC_API_AUTOMATION_SERVER: process.env.NEXT_PUBLIC_API_AUTOMATION_SERVER,
+    },
+  
 };
 
 export default nextConfig;
