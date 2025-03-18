1981b38193dc06ef3093f77dcec4dd616bd405ab
Antonio Alves <antonioalves.dev@outlook.com>
2025-03-18 09:34:47 -0300

    add alter variable

--- src/connection/apiClient.ts
+++ src/connection/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 require('dotenv/config');
-const baseURL =  process.env.NEXT_PUBLIC_API_AUTOMATION_SERVER;
+const baseURL =  process.env.NEXT_PUBLIC_API_AUTOMATION_SERVER || "http://192.168.116.46:8002";
 console.log(baseURL)
 const apiClient = axios.create({
   baseURL: baseURL,
